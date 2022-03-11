8839485a3704954d9f2d3423f6d15939ee52ad85
Theophile Avoyne <tavoyne@gmail.com>
2022-03-11 17:23:55 +0100

    Added meta for posts/$slug

--- app/routes/posts/$slug.tsx
+++ app/routes/posts/$slug.tsx
@@ -1,9 +1,26 @@
 import { Link } from "remix";
 import { useLoaderData } from "remix";
-import type { LoaderFunction } from "remix";
+import type { LoaderFunction, MetaFunction } from "remix";
 import invariant from "tiny-invariant";
 
 import { getPost } from "~/post";
+
+export const meta: MetaFunction = ({
+  data,
+}: {
+  data: Awaited<ReturnType<typeof getPost>> | undefined;
+}) => {
+  if (!data) {
+    return {
+      description: "This page doesn't exist.",
+      title: "Page not found",
+    };
+  }
+  return {
+    description: data.description,
+    title: data.title,
+  };
+};
 
 export const loader: LoaderFunction = ({ params }) => {
   invariant(params.slug, "expected params.slug");
