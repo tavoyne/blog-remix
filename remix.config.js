a021ca4856748377ce14010a0c9a16f860437185
Theophile Avoyne <tavoyne@gmail.com>
2022-03-16 16:47:35 +0100

    Prepare for vercel

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,10 @@
+/* eslint-disable */
+
 /**
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
+  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
+  serverBuildTarget: "vercel",
 };
