ce361f771edcce83b3688cc4e011afff012cbbe4
Theophile Avoyne <tavoyne@gmail.com>
2022-03-16 16:48:51 +0100

    Prepare for vercel

---
+++ server.js
@@ -0,0 +1,6 @@
+/* eslint-disable */
+
+import { createRequestHandler } from "@remix-run/vercel";
+import * as build from "@remix-run/dev/server-build";
+
+export default createRequestHandler({ build, mode: process.env.NODE_ENV });
